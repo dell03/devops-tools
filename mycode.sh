6f1b38cfd2ab4a91f84f600f45196fab1da85ada
aren <arenkester1@gmail.com>
2023-03-12 09:21:30 +0000

    first act

---
+++ mycode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "welcome to gtbank"
+echo "please enter username"
+read username
+echo "please enter your pin"
+read pin
+sleep 10
+echo login successful
+echo welcome 007
+date
+
